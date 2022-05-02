5db45e1664031dea2f2d48cac0252d4f1c8a800d
syon <syon.xiii@gmail.com>
2022-05-02 16:47:40 +0900

    :zap: checkCircular

--- api/lib/Quest.js
+++ api/lib/Quest.js
@@ -39,16 +39,44 @@ module.exports = class Quest {
     const def = JSON.parse(defJson)
     const target = def[questId]
     const socketId = rawSocketId || uuid().slice(0, 8)
-    if (questId === socketId) {
-      throw new Error(`Socket ID: [${socketId}] itself.`)
-    }
-    const exists = target.sockets.some((x) => x.id === socketId)
-    if (exists) {
-      throw new Error(`Socket ID: [${socketId}] Already exists.`)
-    }
+    Quest.checkAddSocket(def, questId, socketId, type)
     const title = '新しいアイテム'
     target.sockets.push({ id: socketId, type, title })
     const outJson = JSON.stringify(def, null, 2)
     fs.writeFileSync('./db/QuestDef.json', outJson)
+  }
+
+  static checkAddSocket(def, questId, socketId, type) {
+    const target = def[questId]
+    // 自分自身を接続対象に指定はNG
+    if (questId === socketId) {
+      throw new Error(`Socket ID: [${socketId}] itself.`)
+    }
+
+    // 既に接続済みのものはNG
+    const exists = target.sockets.some((x) => x.id === socketId)
+    if (exists) {
+      throw new Error(`Socket ID: [${socketId}] Already exists.`)
+    }
+
+    // 循環参照はNG
+    if (type === 'alias') {
+      const originId = questId
+      Quest.checkCircular(def, socketId, originId)
+    }
+  }
+
+  static checkCircular(def, targetId, originId) {
+    console.log('★CHECK:', targetId)
+    if (!def[targetId]) return
+    const subSockets = def[targetId].sockets
+    const subAliases = subSockets.filter((x) => x.type === 'alias')
+    const isLoop = subAliases.some((x) => x.id === originId)
+    if (isLoop) {
+      throw new Error(`Socket ID: [${originId}] in a circular reference.`)
+    }
+    for (const a of subAliases) {
+      Quest.checkCircular(def, a.id, originId)
+    }
   }
 }
