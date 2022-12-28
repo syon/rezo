077f73c9e8b4a58b0a0eae0140b9c598ebe59d4d
syon <syon.xiii@gmail.com>
2022-12-28 18:52:09 +0900

    class Rezo

--- components/Stage.js
+++ components/Stage.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Stage, Layer } from 'react-konva'
 import Rezo from '../lib/Rezo'
-import { $blueprint } from '../store/blueprint'
 
 const StageComponent = () => {
   const [root, setRoot] = React.useState({})
 
   React.useEffect(() => {
-    $blueprint.helper.getRemoteData().then((data) => {
+    Rezo.getRemoteData().then((data) => {
       setRoot(data)
     })
   }, [setRoot])
@@ -16,8 +15,8 @@ const StageComponent = () => {
     setRoot(Rezo.refreshRootOnMoving(root, arg))
   }
 
-  const boxes = $blueprint.helper.createAllBoxes(root.boxes, onMoving)
-  const binds = $blueprint.helper.createAllWires(root)
+  const boxes = Rezo.createAllBoxes(root.boxes, onMoving)
+  const binds = Rezo.createAllWires(root)
 
   return (
     <Stage width={window.innerWidth} height={window.innerHeight} draggable>
