9a851220bbf152d2d2958230246c758ebc5c7dbc
syon <syon.xiii@gmail.com>
2022-12-28 22:22:20 +0900

    computeHandle

--- components/Wire.js
+++ components/Wire.js
@@ -1,10 +1,28 @@
 import { Path } from 'react-konva'
 
+function computeHandleS(apX, zpX) {
+  const center = (apX + zpX) / 2
+  const tension = Math.abs(apX - zpX) * 0.5
+  if (center < apX + 20) {
+    return apX + Math.max(100, tension)
+  }
+  return Math.max(center, apX + tension)
+}
+
+function computeHandleE(apX, zpX) {
+  const center = (apX + zpX) / 2
+  const tension = Math.abs(apX - zpX) * 0.5
+  if (zpX - 20 < center) {
+    return zpX - Math.max(100, tension)
+  }
+  return Math.min(center, zpX - tension)
+}
+
 function computeBezierData({ sp, ep }) {
   const ap = { x: sp.x, y: sp.y }
   const zp = { x: ep.x, y: ep.y }
-  const sh = { x: (ap.x + zp.x) / 2, y: ap.y }
-  const eh = { x: (ap.x + zp.x) / 2, y: zp.y }
+  const sh = { x: computeHandleS(ap.x, zp.x), y: ap.y }
+  const eh = { x: computeHandleE(ap.x, zp.x), y: zp.y }
   const d = `M${ap.x},${ap.y} C${sh.x},${sh.y} ${eh.x},${eh.y} ${zp.x},${zp.y}`
   return d
 }
